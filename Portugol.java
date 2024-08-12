ee5e0fc59e451f54bbf7d54d3072c3c06e06e7b9
Ernandes Oliveira de Lima Júnior <1132024101012@pucgo.edu.br>
2024-08-12 18:47:45 -0300

    Exemplo em portugol

---
+++ Portugol.java
@@ -0,0 +1,32 @@
+package Portugol;
+
+/* Sequência de acontecimentos entre acordar e sair para o trabalho
+
+public class Portugol {
+    1. O som do despertador toca
+    2. Abro os olhos
+    3. Pego o celular
+    4. Desligo o despertador
+    5. Me descubro da coberta
+    6. Me sento na cama
+    7. Me levanto
+    8. Pego meu uniforme de trabalho
+    9. Ando até o banheiro
+    10. Retiro minha roupa
+    11. Entro no box do banheiro
+    12. Tomo banho
+    13. Me enxugo
+    14. Saio do box do banheiro
+    15. Visto minha roupa de trabalho
+    16. Vou até a pia
+    17. Escovo meus dentes
+    18. Arrumo meu cabelo
+    19. Ando até meu quarto
+    20. Pego minha mochila com meus EPIs
+    21. Vou até a porta de entrada
+    22. Destranco a porta
+    23. Abro a porta
+    24. Saio
+    25. Tranco a porta
+}
+ */
